0353b508df8bac232ed095e4e2b83a6a8e6d2e84
Ara <ayaghsizian@gmail.com>
2017-09-13 18:44:10 -0400

    changed instructions about to start express level 3

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-// CodeSchool Express Level 2
-// Create a simple express node app meeting the following requirements
-// Requirements
-// Create an express app.
-// Create index.html in a folder called public.
-// Index.html
-// Include an H1 header
-// Include a form with an empty selection element
-// Create JS file that will make an ajax request to /cities and display each city inside the selection element.
-// Create a /cities route in your app.js file with at least 4 cities.
+/*
+Requirements
+Create an express app.
+Uses the cities app you made in level 2
+A /cities route that will display all cities. (minimum of 5 cities)
+The /cities route should accept a limit query that will send back:
+a certain number of cities
+All cities if 0 is provided or if limit query is omitted
+return a status error if the limit is higher than the number of cities available in the list
+Add a dynamic route to /cities. This should respond with the state that the city resides in.
+Dynamic route should return Not Found status code if the requested city is not available.
+Make sure to also normalize the data sent in the /cities route. The city sent should be sendable in any case and still find the state it’s in. ie Providence and providence should both return Rhode Island.
+Your normalizing of the data should use a middleware function.
+Make sure your /cities route still displays while the other routes can only be accessed by curl currently.
+*/
 
 var express = require('express');
 var app = express();
