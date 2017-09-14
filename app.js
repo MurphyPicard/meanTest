5e4f7c2b3bb47d1afcf59a9f9c4a52ee17f1440e
Ara <ayaghsizian@gmail.com>
2017-09-14 13:24:39 -0400

    set requirements for level 4

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 /*
 Requirements
-Create an express app.
-Uses the cities app you made in level 2
-A /cities route that will display all cities. (minimum of 5 cities)
-The /cities route should accept a limit query that will send back:
-a certain number of cities
-All cities if 0 is provided or if limit query is omitted
-return a status error if the limit is higher than the number of cities available in the list
-Add a dynamic route to /cities. This should respond with the state that the city resides in.
-Dynamic route should return Not Found status code if the requested city is not available.
-Your normalizing of the data should use a middleware function.
-Make sure your /cities route still displays while the other routes can only be accessed by curl currently.
+Using the level 2/3 app
+Add in the ability to add a new city.
+City name
+Containing city
+Should confirm the city and state has values
+State should have at least two characters.
+City should have at least four characters
+Page should update to hide the city
+Update page to make the cities links which return the state name
+Add ability to remove a city
+City should be confirmed prior to removing it
+Response should contain the correct status code
+Page should update with new info
+Additional challenge
+Add additional information to the site:
+Add the additional data points longitude and latitude
+Clickable link should display google maps centered to the longitude and latitude in new tab
+Form should confirm that lat lon are numbers.
 */
 
 const express = require('express');
