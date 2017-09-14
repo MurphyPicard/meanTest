dc7e1d037a690f7371e5a78c37536025e6e6c71e
Ara <ayaghsizian@gmail.com>
2017-09-13 20:22:12 -0400

    added not found status code to any non cities

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ All cities if 0 is provided or if limit query is omitted
 return a status error if the limit is higher than the number of cities available in the list
 Add a dynamic route to /cities. This should respond with the state that the city resides in.
 Dynamic route should return Not Found status code if the requested city is not available.
-Make sure to also normalize the data sent in the /cities route. The city sent should be sendable in any case and still find the state it’s in. ie Providence and providence should both return Rhode Island.
 Your normalizing of the data should use a middleware function.
 Make sure your /cities route still displays while the other routes can only be accessed by curl currently.
 */
@@ -47,7 +46,13 @@ app.get('/cities', function(req, res){
 app.get('/cities/:name', function(req,res){
   // we can use boston or Boston as params
   var state = cities[req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1).toLowerCase()];
-  res.json(state);
+  if(!state){
+    res.status(404).json("Not Found: "+ req.params.name);
+  }else{
+    res.json(state);
+
+  }
+
 
 });
 
